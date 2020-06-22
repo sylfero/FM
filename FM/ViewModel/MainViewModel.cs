b30dcf16dec21502429b5750a8b62b5bdce9856c
Adam Wolszleger <43927273+sylfero@users.noreply.github.com>
2020-06-22 21:21:40 +0200

    Initial commit

---
+++ FM/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using FM.ViewModel.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+    }
+}
