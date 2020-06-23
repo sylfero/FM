d54e3f9bd1e202a53d1b44d6bbd1b58306ae4202
Adam Wolszleger <43927273+sylfero@users.noreply.github.com>
2020-06-24 00:12:45 +0200

    Base for view model

--- FM/Pages/Menu.xaml.cs
+++ FM/Pages/Menu.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace FM.Pages
 {
     /// <summary>
-    /// Interaction logic for Page1.xaml
+    /// Interaction logic for Menu.xaml
     /// </summary>
-    public partial class Page1 : Page
+    public partial class Menu : Page
     {
-        public Page1()
+        public Menu()
         {
             InitializeComponent();
         }
--- FM/ViewModel/MainViewModel.cs
+++ FM/ViewModel/MainViewModel.cs
@@ -2,12 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace FM.ViewModel
 {
     class MainViewModel : ViewModelBase
     {
+        private Page page = new Pages.Menu();
+        public Page Page
+        {
+            get => page;
+            set => SetProperty(ref page, value);
+        }
     }
 }
